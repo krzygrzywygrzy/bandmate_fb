deda5b4eaad6337588f04ad886a972efa816b004
unknown <trikitirem@outlook.com>
2022-02-02 18:06:24 +0100

    firebase setup

---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD37WbVHIwiMCdkNATErZBL3PAa79a3yPo",
+  authDomain: "bandmate-a597b.firebaseapp.com",
+  projectId: "bandmate-a597b",
+  storageBucket: "bandmate-a597b.appspot.com",
+  messagingSenderId: "451050015757",
+  appId: "1:451050015757:web:f1c5c04d9ab02a80ec1944",
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+export default firebase;
