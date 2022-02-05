cf31f09191c4ccccf3826198d7f5d731129787f4
unknown <trikitirem@outlook.com>
2022-02-05 17:42:13 +0100

    <Join/> basic form, styling updates

--- src/routes/auth/Join.tsx
+++ src/routes/auth/Join.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import "./join.css";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+export type JoinInput = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+};
 
 const Join: React.FC = () => {
-  return <div></div>;
+  const { register, handleSubmit } = useForm<JoinInput>();
+  const onSubmit: SubmitHandler<JoinInput> = async (formData) => {
+    //TODO: add user to db
+  };
+
+  return (
+    <div className="container" id="join">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>First enter some primary information</div>
+        <input
+          {...register("name")}
+          placeholder="name..."
+          className="text-input"
+        />
+        <br />
+        <input
+          {...register("surname")}
+          placeholder="surname..."
+          className="text-input"
+        />
+        <br />
+        <br />
+        <br />
+        <div>First enter some primary information</div>
+        <input
+          {...register("email")}
+          placeholder="email..."
+          className="text-input"
+        />
+        <br />
+        <input
+          type="password"
+          {...register("password")}
+          placeholder="password..."
+          className="text-input"
+        />
+        <br />
+      </form>
+      <section></section>
+    </div>
+  );
 };
 
 export default Join;
