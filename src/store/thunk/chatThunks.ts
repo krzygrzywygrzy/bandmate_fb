2ceae5febf370873476ec442bf37e5b7bd4e4556
trikitirem <trikitirem@outlook.com>
2022-02-14 09:52:23 +0100

    loadChats thunk

--- src/store/thunk/chatThunks.ts
+++ src/store/thunk/chatThunks.ts
@@ -1,3 +1,34 @@
-export const loadChats = ()=> {
-  return ()=> {}
+import {ThunkAction, ThunkDispatch} from "redux-thunk";
+import {RootState} from "../store";
+import ChatActions from "../actions/chatActions";
+import {ChatActionType} from "../actions/actionTypes";
+import {collection, query, where, getDocs} from "firebase/firestore";
+import {firestore} from "../../firebase";
+
+export const loadChats = ():
+    ThunkAction<void, RootState, unknown, ChatActions> => {
+  return async (
+      dispatch: ThunkDispatch<RootState, unknown, ChatActions>,
+      getState: () => RootState,
+  ) => {
+    try {
+      dispatch({type: ChatActionType.LOAD});
+
+      const user = getState().user.data;
+      if (!user) throw Error("User not logged in!");
+
+      const matchesQuery = query(
+          collection(firestore, "matches"),
+          where("id", "in", user.matches),);
+      const matchesSnapshots = await getDocs(matchesQuery);
+      matchesSnapshots.forEach((match)=> {
+        //TODO: get data of user
+      })
+
+
+
+    } catch (err: any) {
+      dispatch({type: ChatActionType.ERROR, payload: err});
+    }
+  }
 }
